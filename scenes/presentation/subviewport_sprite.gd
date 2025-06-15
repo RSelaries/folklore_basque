c3a10b229601a78685d159b43c8e2fdcb6e41930
Selaries Raphael <raphael.selaries@ensaama.net>
2025-06-15 16:20:31 +0200

    mémoire

---
+++ scenes/presentation/subviewport_sprite.gd
@@ -0,0 +1,19 @@
+@tool
+class_name SubviewportSprite
+extends Sprite3D
+
+
+func _ready() -> void:
+	_update_texture()
+
+
+func _process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		_update_texture()
+
+
+func _update_texture() -> void:
+	for child in get_children():
+		if child is SubViewport:
+			texture = ViewportTexture.new()
+			texture.viewport_path = child.get_path()
